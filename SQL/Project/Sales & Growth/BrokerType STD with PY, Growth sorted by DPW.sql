001aa97fee5728a04b44d4a8fc40e7a85e5d8a3c
vertikachaudhary9918-design <vertikachaudhary9918@gmail.com>
2025-12-01 17:08:38 +0530

    Add files via upload

---
+++ SQL/Project/Sales & Growth/BrokerType STD with PY, Growth sorted by DPW.sql
@@ -0,0 +1,53 @@
+-- Step 1: Get Top 10 Brokers by CY DPW
+DROP TABLE IF EXISTS #TopBrokers;
+SELECT TOP 50 BrokerMasterName 
+INTO #TopBrokers
+FROM (
+    SELECT 
+        b.BrokerMasterName,
+        SUM(j.GrossWrittenPremiumRptAmt) AS DPW
+    FROM DWH.DIM.DimBroker_SAYA b
+    LEFT JOIN DWH.dbo.SG_Metric1 j ON j.BrokerSk = b.BrokerKey
+    WHERE j.CalendarYear = 2025 
+      AND j.CalendarMonth NOT IN ( 'November')
+      --AND b.BrokerMasterName NOT IN ('Inactive', 'UNKNOWN')
+    GROUP BY b.BrokerMasterName
+) k
+ORDER BY DPW DESC;
+-- Step 2: CY DPW by Broker Segment
+WITH CY AS (
+    SELECT 
+        b.BrokerType,
+        SUM(j.GrossWrittenPremiumRptAmt) AS CY_DPW
+    FROM DWH.DIM.DimBroker_SAYA b
+    LEFT JOIN DWH.dbo.SG_Metric1 j ON j.BrokerSk = b.BrokerKey
+    WHERE j.CalendarYear = 2025 
+      AND j.CalendarMonth NOT IN ( 'November')
+      AND b.BrokerMasterName IN (SELECT BrokerMasterName FROM #TopBrokers)
+    GROUP BY b.BrokerType
+),
+-- Step 3: PY DPW by Broker Segment
+PY AS (
+    SELECT 
+        b.BrokerType,
+        SUM(j.GrossWrittenPremiumRptAmt) AS PY_DPW
+    FROM DWH.DIM.DimBroker_SAYA b
+    LEFT JOIN DWH.dbo.SG_Metric1 j ON j.BrokerSk = b.BrokerKey
+    WHERE j.CalendarYear = 2024 
+      AND j.CalendarMonth NOT IN ( 'November', 'December')
+      AND b.BrokerMasterName IN (SELECT BrokerMasterName FROM #TopBrokers)
+    GROUP BY b.BrokerType
+)
+-- Step 4: Final Output with Growth %
+SELECT 
+    CY.BrokerType,
+    CY.CY_DPW,
+    PY.PY_DPW,
+    CASE 
+        WHEN PY.PY_DPW = 0 THEN NULL
+        ELSE ((CY.CY_DPW - PY.PY_DPW) * 100.0 / PY.PY_DPW)
+    END AS DPW_GrowthPct
+FROM CY
+LEFT JOIN PY ON CY.BrokerType = PY.BrokerType
+ORDER BY CY.CY_DPW DESC;
+ 
